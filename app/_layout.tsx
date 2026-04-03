a09e3637bb23b4b9a2493ae1812e8c328178deec
pradip mandal <pradipmandal@pradip-ka-MacBook-Air.local>
2026-04-03 19:42:35 +0530

    disable new architecture and remove unused experiment flags in app.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { AppDataProvider } from '@/hooks/use-app-data';
